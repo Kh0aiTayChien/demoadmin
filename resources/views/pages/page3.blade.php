602fad7860f216558989a8db8ab6e933377dce5c
Chiến Lê <81791505+Kh0aiTayChien@users.noreply.github.com>
2025-01-24 11:36:27 +0700

    Update page3.blade.php

--- resources/views/pages/page3.blade.php
+++ resources/views/pages/page3.blade.php
@@ -1,4 +1,18 @@
 @extends('layouts.master')
 @section('content')
+<script>
+      const user_token = localStorage.getItem("user_token");
+      fetch('https://lab.amcolab.vn/api/v1/track_user', {
+          method: 'POST',
+          body: JSON.stringify({
+              user_token: user_token,
+          }),
+          headers: { 'Content-Type': 'application/json' }
+      })
+      .then(response => response.json())
+      .then(data => {
+          console.log("API response:", data);
+      });
+      </script>
     This is page 3
 @endsection
